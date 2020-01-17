9f675a897a90560e4d1ab218b11fa129c3addc6b
Aniket Kumar <aniketkumar049@gmail.com>
2020-01-18 00:34:07 +0530

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
-function App() {
-  return (
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
     <div>
-      
-    </div>
-  );
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div> 
+    
+    );
+  }
 }
-
+ 
 export default App;
