8ebfd37aefc941f0c36c7364d89adf6ae1081318
Aniket Kumar <aniketkumar049@gmail.com>
2020-01-20 23:58:00 +0530

    some fixes

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import BuildControl from './BuildControl/BuildControl';
 import classes from './BuildControls.css'
+
+
 const controls = [
     {label: 'Salad', type: 'salad'},
     {label: 'Bacon', type: 'bacon'},
