37d26448f26c3f97c4f570fe8b1045340559fd51
Aniket Kumar <aniketkumar049@gmail.com>
2020-01-22 03:16:56 +0530

    adding navigation component

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+
+import classes from './Toolbar.css'
+import NavigationItems from './NavigationItems/NavigationItems';
+
+const toolbar = (props) => {
+    return ( 
+        <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </header>
+    );
+}
+ 
+export default toolbar; 
