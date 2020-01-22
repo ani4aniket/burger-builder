4aeb7ba1a6e8b1c354d330f61647eba4a5b1e6b0
Aniket Kumar <aniketkumar049@gmail.com>
2020-01-22 17:59:24 +0530

    side drawer toggle added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 
 import classes from './Toolbar.css'
-import NavigationItems from './NavigationItems/NavigationItems';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => {
     return ( 
         <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo/>
-            <nav>
+            <DrawerToggle clicked={props.drawerToggleClicked} />
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
+            <nav className={classes.DesktopOnly}>
                 <NavigationItems/>
             </nav>
         </header>
