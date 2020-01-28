0ebe70495ced8a9f2070b6e1c75e907d66892261
Aniket Kumar <aniketkumar049@gmail.com>
2020-01-28 23:49:14 +0530

    forms setup begin

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -16,7 +16,7 @@ const order = (props) =>{
                 display: 'inline-block',
                 margin: '0 8px',
                 border: '1px solid #ccc',
-                padding: '5px'
+                padding: '5px '
             }}
             key={ig.name}>{ig.name} ({ig.amount})</span>;
     })
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,14 +3,18 @@ import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
+
 import classes from './ContactData.css'
 class ContactData extends Component {
     state = { 
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: 'Aniket Kumar',
+            street: 'Teststreet 1',
+            zipCode: '123456',
+            country: 'India',
+            email: 'test@test.com',
+            deliveryMethod: 'fastest'
         },
         loading: false
      }
@@ -21,16 +25,7 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Aniket Kumar',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '123456',
-                    country: 'India',
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastest'
+            
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -44,10 +39,10 @@ class ContactData extends Component {
 
     render() { 
         let form = (<form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-            <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-            <input className={classes.Input} type="text" name="street" placeholder="Street" />
-            <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+            <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+            <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
+            <Input inputtype="input" type="text" name="street" placeholder="Street" />
+            <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
             <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
         </form>);
         if(this.state.loading){
