e4bee798bdbdd7e90abf4fd974b198b0585898a4
Aniket Kumar <aniketkumar049@gmail.com>
2020-02-17 23:53:47 +0530

    auth reducer added

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -124,7 +124,7 @@ class Auth extends Component {
                 </form>
             <Button 
                 clicked={this.switchAuthModeHandler}
-                btnType="Danger">SWITCH TO {this.state.isSignup ? "SIGNUP" : "SIGNIN"}</Button>
+                btnType="Danger">SWITCH TO {this.state.isSignup ? "SIGNIN" : "SIGNUP"}</Button>
             </div>
          );
     }
